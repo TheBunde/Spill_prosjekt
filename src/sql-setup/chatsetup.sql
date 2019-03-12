b730931b5a52419d1b108d3c085b5c82f3aedbf8
williad <williad@stud.ntnu.no>
2019-03-12 10:13:56 +0100

    Fixed position of parantheses

--- src/sql-setup/chatsetup.sql
+++ src/sql-setup/chatsetup.sql
@@ -1,23 +1,20 @@
 CREATE TABLE chat(
     chat_id INTEGER NOT NULL AUTO_INCREMENT,
-    CONSTRAINT chat_pk PRIMARY KEY(chat_id)
-);
+    CONSTRAINT chat_pk PRIMARY KEY(chat_id));
 
 CREATE TABLE chatter(
     chatter_id INTEGER NOT NULL AUTO_INCREMENT,
     chat_id INTEGER,
     name VARCHAR(30) NOT NULL,
-    CONSTRAINT chatter_pk PRIMARY KEY(chatter_id)
-);
+    CONSTRAINT chatter_pk PRIMARY KEY(chatter_id));
 
 CREATE TABLE chat_message(
     chat_id INTEGER NOT NULL,
     message_id INTEGER NOT NULL,
     chatter_id INTEGER NOT NULL,
     message VARCHAR(30) NOT NULL,
-    time_stamp DATE NOT NULL
-    CONSTRAINT chat_message_pk PRIMARY KEY(chat_id, message_id)
-);
+    time_stamp DATE NOT NULL,
+    CONSTRAINT chat_message_pk PRIMARY KEY(chat_id, message_id));
 
 ALTER TABLE chatter
   ADD CONSTRAINT chatter_fk FOREIGN KEY(chat_id) REFERENCES chat(chat_id);
