514099631fbde9e1d3afd16303a215e2f4aacbcb
magnubau <magnubau@stud.ntnu.no>
2019-03-13 16:17:24 +0100

    Removed unused imports

--- src/interface/sample/LoginController.java
+++ src/interface/sample/LoginController.java
@@ -1,11 +1,7 @@
 package sample;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
 
 
 public class LoginController {
