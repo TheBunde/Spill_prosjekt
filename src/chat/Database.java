2b91d217cdae3dbd464dbab5a97322064cf9ae49
shahedsa <shahedsa@stud.ntnu.no>
2019-03-14 11:15:05 +0100

    Test

--- src/chat/Database.java
+++ src/chat/Database.java
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-
+//test
 public class Database {
     private Connection con;
     private String url;
