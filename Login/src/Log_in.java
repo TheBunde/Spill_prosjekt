4e9be49850b7ae47bba70b91f464ec80549aebb9
shahedsa <shahedsa@stud.ntnu.no>
2019-03-14 10:52:34 +0100

    hello

--- Login/src/Log_in.java
+++ Login/src/Log_in.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
-
+import java.awt.event.*;
+import java.sql.*;
 public class Log_in {
+    private Connection connection;
+    private Statement st;
+    private ResultSet res;
+
+    private Log_in(){
+        // her skal vi åpne kobling til database.
+
+
+    }
+
+    public void connect(){
+        try{
+            String driver ="mysql-ait.stud.idi.ntnu.no ";
+            Class.forName(driver);
+
+            String db ="g_tdat1006_01";
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
 }
