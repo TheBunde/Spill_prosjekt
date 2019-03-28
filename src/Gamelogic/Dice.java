82163b0d3020a396d837b341512fc801d0757184
heleneyj <heleneyj@stud.ntnu.no>
2019-03-28 14:27:41 +0100

    fixes

--- src/Gamelogic/Dice.java
+++ src/Gamelogic/Dice.java
@@ -3,14 +3,18 @@ package Gamelogic;
 import java.util.Random;
 class Dice {
     private int dice;
+    private int amount;
 
     public Dice(){
         this.dice = dice;
     }
 
-    public int roll(int dice){
+    public int roll(int dice, int amount){
+        int number = 0;
         Random random = new Random();
-        int number = random.nextInt(dice) + 1;
+        for(int i = 0; i < amount; i++ ) {
+            number += random.nextInt(dice) + 1;
+        }
         return number;
     }
 }
