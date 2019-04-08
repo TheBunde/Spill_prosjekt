d9bb937ec0e7ebd55816d76918ff4a4bfc449353
magnubau <magnubau@stud.ntnu.no>
2019-04-08 16:04:51 +0200

    updating

--- test/game/MonsterTest.java
+++ test/game/MonsterTest.java
@@ -30,9 +30,16 @@ class MonsterTest{
         m1weapons.add(m1w2);
         Monster m1 = new Monster(4, 8, "Ushabti", 20, 10, 3, 6, 5, "", 8, 6, null, m1weapons);
 
+        ArrayList<Weapon> m2weapons = new ArrayList<>();
+        Weapon m2w1 = new Weapon("Claw", 6, false, 2, "");
+        m1weapons.add(m1w1);
+        Monster m2 = new Monster(3, 5, "Bear", 20, 10, 3, 6, 5, "", 8, 5, null, m1weapons);
+
+        creatures = new ArrayList<>();
         creatures.add(c1);
         creatures.add(c2);
         creatures.add(m1);
+        creatures.add(m2);
     }
 
     @AfterEach
@@ -42,10 +49,22 @@ class MonsterTest{
 
     @Test
     void toStringTest() {
+        Monster m1 = (Monster) creatures.get(2);
+        String expected1 = "Character: Ushabti\nHP: 20\nAC: 10\nMovement: 3\nWeapon: Dagger, Greatbow\nAttack bonus: 5\nBackstory: ";
+        assertEquals(expected1, m1.toString(), "toString method did not output correct values");
+
+        //Altering some values
+        m1.setHp(30);
+        m1.addNewWeapon(new Weapon("Javelin", 8, true, 1, ""));
+
+        String expected2 = "Character: Ushabti\nHP: 30\nAC: 10\nMovement: 3\nWeapon: Dagger, Greatbow, Javelin\nAttack bonus: 5\nBackstory: ";
+        //Checking for change in result
+        assertEquals(expected2, m1.toString(), "toString method did not output correct values after change");
     }
 
     @Test
     void monsterMove() {
+
     }
 
     @Test
@@ -74,14 +93,41 @@ class MonsterTest{
 
     @Test
     void moveTo() {
+        Monster m1 = (Monster) creatures.get(2);
+        Character c1 = (Character) creatures.get(0);
+        Monster m2 = (Monster) creatures.get(3);
+        c1.setNewPos(12, 2);
+        m1.setNewPos(10, 4);
+        m1.moveTo(c1, creatures);
+         // Checking if m1 moves next to c1
+        assertTrue(m1.getxPos() == 11, "Did not move to right place");
+        assertTrue(m1.getyPos() == 3, "Did not move to right place");
+        m1.setNewPos(10, 4);
+        m2.setNewPos(11, 3);
+        m1.moveTo(c1, creatures);
+         // Checking if m1 does not move on top of m2
+        assertTrue(m1.getxPos() == 11 && m1.getyPos() == 2, "Did not move away from m2 in the correct way");
+
     }
 
     @Test
     void dontStepOnOthers() {
+        Monster m1 = (Monster) creatures.get(2);
+        Character c1 = (Character) creatures.get(0);
+        c1.setNewPos(9, 9);
+        m1.setNewPos(10, 9);
+        ArrayList<Integer> pos = m1.dontStepOnOthers(m1.getxPos(), m1.getyPos(), c1);
+        m1.setNewPos(pos.get(0), pos.get(1));
+         // Checking if the monster moves one grid down
+        assertTrue(m1.getxPos() == 10 && m1.getyPos() == 10, "Did not step in the right direction");
+        c1.setNewPos(0, 0);
     }
 
     @Test
     void direction() {
+        Monster m1 = (Monster) creatures.get(2);
+        int direction = m1.direction(10, 5);
+        assertTrue(direction == -1, "Method gives wrong direction");
     }
 
     @Test
