1118be71cf948a8af2675413edc551741b804775
magnubau <magnubau@stud.ntnu.no>
2019-04-08 14:49:43 +0200

    updating

--- test/game/MonsterTest.java
+++ test/game/MonsterTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MonsterTest extends CreatureTest {
+class MonsterTest{
 
     @BeforeEach
     void setUp() {
