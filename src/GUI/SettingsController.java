c2d2cbecc9810cf7cf8671a2007d1af0d56bae19
Henrik Wanderås Tronstad <henrikwt@stud.ntnu.no>
2019-04-11 21:27:49 +0200

    Update SettingsController.java

--- src/GUI/SettingsController.java
+++ src/GUI/SettingsController.java
@@ -4,7 +4,9 @@ import audio.MusicPlayer;
 import audio.SFXPlayer;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-
+/**
+ * Controller class for the settings menu. Allows users to adjust the volume of the audio.
+ */
 public class SettingsController {
 
     @FXML
@@ -15,17 +17,24 @@ public class SettingsController {
     private Slider sfxSlider;
     @FXML
     private SceneSwitcher sceneSwitcher;
-
+    /**
+     * Constructor.
+     */
     public SettingsController(){
         sceneSwitcher = new SceneSwitcher();
     }
-
+    /**
+     * Takes the user back to the main menu when the designated button is pressed.
+     * @throws Exception
+     */
     public void backToMenu() throws Exception{
         SFXPlayer.getInstance().setSFX(0);
         sceneSwitcher.switchScene(backToMenuButton, "MainMenu.fxml");
 
     }
-
+    /**
+     * Adjusts the volume of the music with a slider button.
+     */
     public void changeVolumeMusic(){
 
         double slideValue = musicSlider.getValue();
@@ -44,7 +53,9 @@ public class SettingsController {
 
 
     }
-
+    /**
+     * Adjusts the volume of the sound effects with a slider button.
+     */
     public void changeVolumeSFX(){
 
         double slideSFXValue = sfxSlider.getValue();
