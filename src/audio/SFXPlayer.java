410b4022538a139ab44ca0715c28911b6e0a47ca
Henrik Wanderås Tronstad <henrikwt@stud.ntnu.no>
2019-04-11 21:39:57 +0200

    Added javadoc comments SFXPlayer.java

--- src/audio/SFXPlayer.java
+++ src/audio/SFXPlayer.java
@@ -2,13 +2,20 @@ package audio;
 
 import java.util.ArrayList;
 
-public class SFXPlayer implements Runnable{
+/**
+ * This class used to make a playlist of sound effects (SFX)
+ * to be used in the game. The class contains
+ * methods to manipulate and access the SFX when required.
+ * @author henrikwt
+ */
 
-    private ArrayList<AudioFile> fileList;
+public class SFXPlayer implements Runnable{
 
     private int currentSFXIndex;
     private float volSFX;
+    private ArrayList<AudioFile> fileList;
 
+    /* All the SFX names get added in this SFXPlayer object. */
     private static SFXPlayer thisInstance = new SFXPlayer("knockSFX", "splashSFX","warriorSFX","rogueSFX2","wizardSFX",
         "noKeySFX","wrongKeySFX","correctKeySFX","rangerSFX","arrowSFX","enmhitSFX","enmmissSFX","pdefSFX","victory1SFX"
     ,"defeat", "moveSFX","gameWonSFX");
@@ -18,43 +25,60 @@ public class SFXPlayer implements Runnable{
     }
 
     /*
-    List of SFX and index:
-    0: "knockSFX"
-    1: "splashSFX"
-    2: "warriorSFX"
-    3: "rogueSFX2"
-    4: "wizardSFX"
-    5: "noKeySFX"
-    6: "wrongKeySFX"
-    7: "correctKeySFX"
-    8: "rangerSFX"
-    9: "arrowSFX"
-    10: "enmhitSFX"
-    11: "enmmissSFX"
-    12: "pdefSFX"
-    13: "victory1SFX"
-    14: "defeat"
-    15: "moveSFX"
-    16: "gameWonSFX"
+     * List of SFX and index:
+     * 0: "knockSFX"
+     * 1: "splashSFX"
+     * 2: "warriorSFX"
+     * 3: "rogueSFX2"
+     * 4: "wizardSFX"
+     * 5: "noKeySFX"
+     * 6: "wrongKeySFX"
+     * 7: "correctKeySFX"
+     * 8: "rangerSFX"
+     * 9: "arrowSFX"
+     * 10: "enmhitSFX"
+     * 11: "enmmissSFX"
+     * 12: "pdefSFX"
+     * 13: "victory1SFX"
+     * 14: "defeat"
+     * 15: "moveSFX"
+     * 16: "gameWonSFX"
      */
 
-    public SFXPlayer(String... files){
+    /**
+     * Constructor for SFXPlayer.
+     * @param sfxFiles SFX Files.
+     */
+    public SFXPlayer(String... sfxFiles){
         fileList = new ArrayList<AudioFile>();
-        for(String file : files)
-            fileList.add(new AudioFile(file + ".wav"));
+        for(String sfxFile : sfxFiles)
+            fileList.add(new AudioFile(sfxFile + ".wav"));
     }
 
-    public void setVolSFX(float volSFX){
-        this.volSFX = volSFX;
-    }
-
-    public void setSFX(int currentSFXIndex){
-        AudioFile sfx = fileList.get(currentSFXIndex);
-        sfx.play(volSFX);
-    }
+    /**
+     * run() method.
+     */
     @Override
     public void run(){
         AudioFile soundEffect = fileList.get(currentSFXIndex);
         soundEffect.play(volSFX);
     }
+
+    /**
+     * Sets the volume of the SFX.
+     * @param volSFX Volume.
+     */
+    public void setVolSFX(float volSFX){
+        this.volSFX = volSFX;
+    }
+
+    /**
+     * Plays the SFX.
+     * @param currentSFXIndex Index of SFX.
+     */
+    public void setSFX(int currentSFXIndex){
+        AudioFile sfx = fileList.get(currentSFXIndex);
+        sfx.play(volSFX);
+    }
 }
+
