5b806a0d67a745447ed9e8d524717571c4080c7a
Henrik Wanderås Tronstad <henrikwt@stud.ntnu.no>
2019-04-11 21:44:48 +0200

    Added javadoc comments AudioFile.java

--- src/audio/AudioFile.java
+++ src/audio/AudioFile.java
@@ -12,16 +12,26 @@ import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineEvent;
 import javax.sound.sampled.LineListener;
 
+/**
+ * This class is used to make audio files playable.
+ * @author henrikwt
+ */
+
 public class AudioFile implements LineListener{
 
-    private InputStream is;
-    private AudioInputStream ais;
-    private AudioFormat audioFormat;
-    private DataLine.Info DLinfo;
-    private Clip soundClip;
-    private FloatControl fControl;
-    private volatile boolean play;
 
+    private AudioFormat audioFormat;
+    private AudioInputStream ais;
+    private volatile boolean play;
+    private Clip soundClip;
+    private DataLine.Info DLinfo;
+    private FloatControl fControl;
+    private InputStream is;
+
+    /**
+     * Constructor for AudioFile.
+     * @param fileName Name of the file.
+     */
     public AudioFile(String fileName) {
         is = Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName);
         try {
@@ -32,45 +42,59 @@ public class AudioFile implements LineListener{
             soundClip.addLineListener(this);
             soundClip.open(ais);
             fControl = (FloatControl) soundClip.getControl(FloatControl.Type.MASTER_GAIN);
-            ais.close();
-            is.close();
 
         } catch (Exception exc) {
             exc.printStackTrace();
-            System.exit(1);
-
         }
     }
-    public void play() {
-        play(-9);
-    }
 
-    public void play(float volume) {
-        fControl.setValue(volume);
+    /**
+     * Plays the audio, with set volume parameter.
+     * @param db Value in decibel to add or subtract from original volume.
+     */
+    public void play(float db) {
+        fControl.setValue(db);
         soundClip.start();
         play = true;
     }
 
+    /**
+     * Plays the audio, with a standard volume.
+     */
+    public void play() {
+        play(-9);
+    }
+
+    /**
+     * Stops the audio.
+     */
+    public void audioStop(){
+        soundClip.stop();
+        soundClip.flush();
+        play = false;
+    }
+
+    /**
+     * Checks if the audio is playing or not.
+     * @return Whether the audio is playing or not.
+     */
     public boolean isPlay() {
         return play;
     }
 
-    public void stopMusic(){
-        soundClip.stop();
-        soundClip.flush();
-        soundClip.setFramePosition(0);
-        play = false;
-    }
+    /**
+     * Line Event update.
+     * @param lEvent Line Event.
+     */
     @Override
-    public void update(LineEvent event) {
-        if(event.getType() == LineEvent.Type.START)
+    public void update(LineEvent lEvent) {
+        if(lEvent.getType() == LineEvent.Type.START)
             play = true;
-        else if(event.getType() == LineEvent.Type.STOP) {
+        else if(lEvent.getType() == LineEvent.Type.STOP) {
             soundClip.stop();
             soundClip.flush();
             soundClip.setFramePosition(0);
             play = false;
         }
     }
-
 }
