2b5755a7dee8e01184c0077704522e25d35ced2e
Henrik Wanderås Tronstad <henrikwt@stud.ntnu.no>
2019-04-11 21:43:25 +0200

    Added javadoc comments IDAssigner.java

--- src/audio/IDAssigner.java
+++ src/audio/IDAssigner.java
@@ -1,11 +1,14 @@
 package audio;
+
 /**
  * This class is used to make a base ID which can be utilized by the thread classes.
  * @author henrikwt
  */
 public class IDAssigner {
 
+
     private int bID;
+
     /**
      * Constructor.
      * @param bID Base ID.
@@ -13,18 +16,21 @@ public class IDAssigner {
     public IDAssigner(int bID) {
         this.bID = bID;
     }
-    /**
-     * Increments the Base ID.
-     * @return Base ID + 1;
-     */
-    public int next() {
-        return bID++;
-    }
+
     /**
      * Get method.
      * @return Base ID
      */
-    public int getID() {
+    public int getBID() {
         return bID;
     }
+
+    /**
+     * Increments the Base ID.
+     * @return Base ID + 1;
+     */
+    public int nextBID() {
+        return bID++;
+    }
+
 }
