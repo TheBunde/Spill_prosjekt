167fac2fb7428e89b8a434b67721a7c8b9607f50
Sara Mohammadi <saramoh@stud.ntnu.no>
2019-04-11 08:26:58 +0200

    Update AccountDetailsController.java

--- src/GUI/AccountDetailsController.java
+++ src/GUI/AccountDetailsController.java
@@ -11,8 +11,6 @@ import javafx.scene.text.Text;
  * The program handles the components in AccountDetails-scene.
  * @author saramoh
  */
-
-
 public class AccountDetailsController {
     private SceneSwitcher sceneSwitcher;
 
@@ -23,59 +21,57 @@ public class AccountDetailsController {
     private Button changeUsernameButton, changePasswordButton, backButton;
 
 
+    /**
+     * Constructor for AccountDetailsController
+     */
     public AccountDetailsController() {
         sceneSwitcher = new SceneSwitcher();
     }
-    
-     /**
+
+    /**
      * Initializes information from the method getInfo()
+     *
      * @throws Exception
      */
-
-
     public void initialize() throws Exception {
         getInfo();
     }
-    
-     /**
+
+    /**
      * Sets the Username and the Rank which is fetched from DB as a fxml-text
+     *
      * @throws Exception
      */
-
-
     public void getInfo() throws Exception {
         usernameOutput.setText(Main.db.fetchUsername());
         rankOutput.setText("Rank: " + Main.db.fetchRank(Main.user.getUser_id()));
-
     }
-   
+
     /**
-     * Switches the scene to the ChangeUsername-scene.
+     * Switches the scene to the changeUsername-scene
+     *
      * @throws Exception
      */
-
     public void changeUsernameButtonPressed() throws Exception {
         SFXPlayer.getInstance().setSFX(0);
         sceneSwitcher.switchScene(changeUsernameButton, "changeUsername.fxml");
     }
-    
-     /**
-     * Switches the scene to the ChangePassword-scene.
+
+    /**
+     * Switches the scene to the changePassword-scene
+     *
      * @throws Exception
      */
-
-
     public void changePasswordButtonPressed() throws Exception {
         SFXPlayer.getInstance().setSFX(0);
         sceneSwitcher.switchScene(changePasswordButton, "changePassword.fxml");
     }
-    
-     /**
-     * Switches the scene to the MainMenu-scene.
+
+    /**
+     * Switches the scene to the MainMenu-scene
+     *
      * @throws Exception
      */
-
-
     public void backToMenuButtonPressed() throws Exception {
         SFXPlayer.getInstance().setSFX(0);
         sceneSwitcher.switchScene(backButton, "MainMenu.fxml");
